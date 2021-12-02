5cd6b84232058f64320fadd056c6f59e5eb1ead5
jonathanBidmead <80360821+jonathanBidmead@users.noreply.github.com>
2021-12-02 15:10:44 +1300

    major changes to PI controller
    Made it so that the working control output jumps between +/- some minPower value. This eliminates the long integral windup time as the control effort crossed the deadzone. The machine can now follow a sinusoid fairly well, with some caveats.
    - physical assembly had some slop between the 2nd arm and its large gear
    - slop also present in motor (inevitable)
    - vibrations at end effector loosened the screws on the solenoid, further worsening matters.
    - can the controller be reworked such that its output is smooth?
    - static friction is a bitch, meaning the controller must wait for large integral windup before starting, but then immediately overshoots as it becomes unstuck. There's probably some advanced fuckery to deal with it though.
    - whole assembly droops hugely. Need to add more support at base somehow. Easy retrofit option: two large nuts that can be twisted into place.

--- PI_controller/PI_controller.ino
+++ PI_controller/PI_controller.ino
@@ -2,21 +2,31 @@
 const byte encoder0pinA = 2;//A pin -> the interrupt pin 0
 const byte encoder0pinB = 3;//B pin -> the digital pin 3
 byte encoder0PinALast;
+
+
 float pos = 0;
 float vel = 0;
-float delayTime = 100;
+float delayTime = 10;
 int duration;//the number of the pulses
 boolean Direction;//the rotation direction
 
-float refPos = 2;
-float KP = 60;
-float KI = 20;
+float refPos = 0.3;
+float KP;
+float KI;
+
+
 
 float error = 0;
 float errorSum = 0;
 float ctrl = 0;
-float minPower = 0;
+float minPower = 100;
 float epsilon = 0.05;
+float maxPower = 200;
+
+float startTime = 0;
+float currentTime = 0;
+int stepTime = 4000;
+
 void setup()
 {
   Serial.begin(57600);//Initialize the serial port
@@ -27,86 +37,102 @@ void setup()
   analogWrite(9,0);
   digitalWrite(12,LOW);//positive direction by default
   digitalWrite(13,HIGH);
+  
+  //good for 0.3 sqaure
+  //KP = 120;
+  //KI = 1;
+
+  KP = 400;
+  KI = 3;
+  
+  
+  
   delay(2000);
+  startTime = millis();
 }
 
 void loop()
 {
-  //generating fluctuating reference
-//  refPos = 1 + 1*sin(0.0003*(millis()-2100));
-  if (abs(error) < epsilon){
-    refPos = 2-refPos;
-    errorSum = 0;
-  }
-//  refPos = 3;
+  //generating fluctuating references
+  //sinusoidal reference
+  refPos = 0.3*sin(0.0005*(millis()-2100));
+
+  //square wave reference
+//  currentTime = millis();
+//  if ((currentTime - startTime) > stepTime){
+//    if (refPos == 0.3){
+//      refPos = -0.3;
+//    }
+//    else if (refPos == -0.3){
+//      refPos = 0.3;
+//    }
+//    stepTime += 4000;
+//    errorSum = 0;
+//  }
+
+
+
+  //calculating variables
   vel = duration;
   pos += duration;
+  duration = 0;
   error = refPos - pos/(341.2*2);
-//  errorSum += error;
+  errorSum += error;
 
-  //saturating integral action (only increase if below threshold)
-  if (abs(error) < 1 && abs(KI*errorSum) < 50){
-    errorSum += error;
-  }
+//  //limiting effect of integral action
+//  if (abs(error) > 0.05){
+//    errorSum -= error;
+//  }
 
   //calculate control
   ctrl = KP*error + KI*errorSum;
   
-  //changing direction
+  //changing direction and applying deadzone offset
   if(ctrl < 0){
-//    analogWrite(9,0);
-    
     digitalWrite(12,HIGH);//reversing
     digitalWrite(13,LOW);
-//    delay(1);
+    ctrl -= minPower;
   }
   else {
-//    analogWrite(9,0);
-    
     digitalWrite(12,LOW);//forwards
     digitalWrite(13,HIGH);
-//    delay(1);
+    ctrl += minPower;
   }
 
   //saturating control
-  if (abs(ctrl) > 255){
+  if (abs(ctrl) > maxPower){
     Serial.println("Saturating");
     if (ctrl < 0){
-      ctrl = -255;
+      ctrl = -maxPower;
     }
     else{
-      ctrl = 255;
-    }
-  }
-  if (abs(ctrl) < minPower){
-    if (ctrl < 0){
-      ctrl = -minPower;
-    }
-    else {
-      ctrl = minPower;
+      ctrl = maxPower;
     }
   }
 
-  //turning off control if setpoint within acceptable limits
-//  if (abs(error) < epsilon){
-//    ctrl = 0;
-//    error = refPos - pos/(341.2*2);
-//  }
-  
+  //sending working control to motor
   analogWrite(9,abs(ctrl));
+//  analogWrite(9,100*sin(0.0005*(millis()-2100)));
   
   Serial.print("Position,Reference,Control,Velocity:");
-  Serial.print(pos/(341.2*2));
+  Serial.print(100*pos/(341.2*2) -300);
   Serial.print(",");
-  Serial.println(refPos);
-//  Serial.print(",");
-//  Serial.println(ctrl);
+  Serial.print(100*refPos -300);
+  Serial.print(",");
+  Serial.print(minPower);
+  Serial.print(",");
+  Serial.print(-minPower);
+  Serial.print(",");
+  Serial.println(ctrl);
 //  Serial.print(",");
 //  Serial.println(vel/(341.2*2*(delayTime/1000)));
 
 //  Serial.print("Control: ");
 //  Serial.println(ctrl);
-  duration = 0;
+
+
+
+  
   delay(delayTime);
 }
 
